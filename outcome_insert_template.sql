6c42977a0c14b3f5f7075f920d94fcc9072d28dc
Goose <goosemixes@gmail.com>
2019-06-18 21:02:32 -0400

    Outcome insert template
    This is all I'm doing today because laaaazy

---
+++ outcome_insert_template.sql
@@ -0,0 +1,29 @@
+INSERT INTO `outcome` (
+`offense_play_key`,
+`name`,
+`announcer_text`,
+`yards`,
+`is_complete`,
+`is_incomplete`,
+`is_interception`,
+`is_fumble`,
+`is_turnover`,
+`is_kick_good`,
+`is_kick_no_good`,
+`is_always_td`,
+`is_always_defense_td`,
+) VALUES (
+1, -- offense_play_key
+'', -- name
+'', -- announcer_text
+25, -- yards
+0, -- is_complete
+0, -- is_incomplete
+0, -- is_interception
+0, -- is_fumble
+0, -- is_turnover
+0, -- is_kick_good
+0, -- is_kick_no_good
+0, -- is_always_td
+0, -- is_always_defense_td
+);
